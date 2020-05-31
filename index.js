2ce07e1076e3340c9ec88bd36da39282ae865ed0
SantiagoDuque1700 <duquemoreno12@gmail.com>
2020-05-31 18:32:07 -0500

    obteniendo data

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const puppeteer = require('puppeteer');
 
-const db = []
+const db = {
+    pokemons: {},
+    types: {}
+}
 
 const getDataFromPuppeteer = async (initialData) => {
   try {
-    const info = [];
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://pokemon.fandom.com/es/wiki/Lista_de_Pok%C3%A9mon_de_la_primera_generaci%C3%B3n', { waitUntil: 'load', timeout: 0 });
 
-    await page.evaluate(() => {
+    const list = await page.evaluate(() => {
+      const data = []  
       const table = document.getElementsByClassName('tabpokemon')[0].children[1];
-
+        console.log(table)
       // eslint-disable-next-line no-restricted-syntax
       for (const children of table.children) {
-        
+        const type2 = children.children[4].children[0] ? true : false
+        const pokemon = {
+            name: children.children[2].innerText,
+            type: children.children[3].children[0].title,
+            type2: type2 ? children.children[4].children[0].title : ''
+        }
+        data.push(pokemon)
       };
 
-      return data;
+      return data
     });
 
-    console.log(list)
+    db.pokemons = list
+    console.table(db.pokemons)
     await browser.close();
   } catch (error) {
     // eslint-disable-next-line no-console
@@ -30,4 +40,3 @@ const getDataFromPuppeteer = async (initialData) => {
 };
 
 getDataFromPuppeteer()
-console.table(db)
